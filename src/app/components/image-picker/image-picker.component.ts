d025e87eb073790ffcf31334078b64164363f7ed
Naveen Kumar Veerapalli <naveenvicky770@gmail.com>
2021-11-24 16:28:07 +0530

    Image pickers using capacitor added

--- src/app/components/image-picker/image-picker.component.ts
+++ src/app/components/image-picker/image-picker.component.ts
@@ -1,6 +1,8 @@
+/* eslint-disable prefer-const */
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Camera,CameraResultType, CameraSource } from '@capacitor/camera';
+import {CameraSource } from '@capacitor/camera';
 import { Plugins, Capacitor } from '@capacitor/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-image-picker',
@@ -17,26 +19,25 @@ export class ImagePickerComponent implements OnInit {
   ngOnInit() {}
 
   onPickImage(){
-    if(!Capacitor.isPluginAvailable('Camera')){
-      console.log('Camera Plugin not available');
-      return;
-    }
 
-    Plugins.Camera.getPhoto({
-      quality: 50,
-      source: CameraSource.Prompt,
-      correctOrientation: true,
-      height: 320,
-      width:200,
-      resultType: CameraResultType.Base64
-    }).then( image => {
-      this.selectedImage = image.base64Data;
-      this.imagePick.emit(image.base64Data);
+    const takePicture = async () => {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri
+      });
 
-    }).catch( err => {
-      console.log(err);
-      return false;
-    });
+      // image.webPath will contain a path that can be set as an image src.
+      // You can access the original file using image.path, which can be
+      // passed to the Filesystem API to read the raw data of the image,
+      // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+      let imageUrl = image.webPath;
+
+      // Can be set to the src of an image now
+      this.selectedImage = imageUrl;
+    };
+
+    takePicture();
 
   }
 
