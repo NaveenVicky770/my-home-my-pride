c2ba48b73c0917a88dbe00e6d2b0a7650d347f25
Naveen Kumar Veerapalli <naveenvicky770@gmail.com>
2022-02-09 17:40:09 +0530

    Payment for a specific location page changes

--- src/app/pages/multilocations-payment/multilocations-payment.page.ts
+++ src/app/pages/multilocations-payment/multilocations-payment.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-multilocations-payment',
@@ -11,11 +12,20 @@ export class MultilocationsPaymentPage implements OnInit {
   customPopoverOptions: any = {
     message: 'Select location',
   };
-  locations=['location1','location2','location3'];
+  locations=[];
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.getExistingHouses().subscribe((resObj) => {
+      console.log('Existing Houses', resObj);
+      resObj.message.forEach((element) => {
+        this.locations.push({
+          houseName: element.new_house_name,
+          houseId: element.location_id,
+        });
+      });
+    });
   }
 
 
